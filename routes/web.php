8c065766f2f3e51de0667134303332ec118d5bef
AnnemetteBP <ampirchert@gmail.com>
2018-12-11 23:24:45 +0100

    Posts and admin backend

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Auth::routes();
 Route::prefix('/admin')->middleware(['auth'])->group(function(){
     Route::get('/', [AdminController::class, 'index']);
     Route::get('/home', [AdminController::class, 'index'])->name('home');
+    Route::get('/read/{id}', [AdminController::class, 'read'])->name('read');
     Route::post('/create', [AdminController::class, 'createPost'])->name('create');
     Route::put('/update/{id}', [AdminController::class, 'updatePost'])->name('update');
     Route::delete('/delete/{id}', [AdminController::class, 'deletePost'])->name('delete');
